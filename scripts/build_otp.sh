ca8d3967745f3bbf8e736b930dfd718596b00f18
Aaron Shea <ashea@synopsys.com>
2021-11-10 12:02:16 -0800

    add apple silicon

--- scripts/build_otp.sh
+++ scripts/build_otp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+##########
+## Initial Prep
+
 set -e
 version=$1
 
@@ -8,10 +11,34 @@ if [ -z "${version}" ]; then
   exit 1
 fi
 
-OpenSSL_VERSION="1.1.1l"
-OpenSSL_DIR="$HOME/openssl_prefix"
-mkdir -p $OpenSSL_DIR
 mkdir -p $TMPDIR
+
+##########
+## OpenSSL
+
+OpenSSL_VERSION="1.1.1i"
+OpenSSL_DIR="$HOME/openssl_prefix"
+
+mkdir -p $OpenSSL_DIR
+
+echo "building OpenSSL $OpenSSL_VERSION"
+curl https://www.openssl.org/source/openssl-$OpenSSL_VERSION.tar.gz -O
+tar -xzf openssl-$OpenSSL_VERSION.tar.gz
+cd openssl-$OpenSSL_VERSION
+
+if [[ $APPLE_SILICON == 1 ]]
+then
+  CC="clang -target arm64-apple-macos11" ./Configure darwin64-arm64-cc no-shared --prefix="$OpenSSL_DIR" && make depend && make -j && make install
+else
+  ./config no-shared --prefix="$OpenSSL_DIR" && make depend && make && make install
+fi
+
+##########
+## OTP
+
+echo "building OTP ${version}"
+
+OTP_BUILD_FLAGS="--disable-parallel-configure --without-jinterface --without-hipe --disable-dynamic-ssl-lib --with-ssl=$OpenSSL_DIR"
 
 # Workaround for drawin compiler flags on some builds of OTP
 if [[ $(uname -s) == Darwin ]]
@@ -22,20 +49,12 @@ then
     export CPP="clang -E"
 fi
 
-# --disable-parallel-configure is to get around a bug in autoconf for MacOS...
-OTP_BUILD_FLAGS="--disable-parallel-configure --without-jinterface --without-hipe --disable-dynamic-ssl-lib --with-ssl=$OpenSSL_DIR"
-
-echo "building OpenSSL $OpenSSL_VERSION"
-curl https://www.openssl.org/source/openssl-$OpenSSL_VERSION.tar.gz -O && \
-    tar -xzf openssl-$OpenSSL_VERSION.tar.gz && \
-    cd openssl-$OpenSSL_VERSION && ./config no-shared --prefix="$OpenSSL_DIR" && make depend && make && make install
-
-echo "building OTP ${version}"
-
 TMPDIR="${TMPDIR:=tmp}/beamup"
 mkdir -p $TMPDIR
 echo "cd $TMPDIR"
 cd $TMPDIR
+
+export arch="$(uname -sm | tr '[:upper:]' '[:lower:]' | tr ' ' '-')"
 
 if [ ! -f otp_src_${version}.tar.gz ]; then
   url=https://github.com/erlang/otp/releases/download/OTP-${version}/otp_src_${version}.tar.gz
@@ -48,20 +67,35 @@ if [ ! -d otp_src_${version} ]; then
   tar xzf otp_src_${version}.tar.gz -C otp_src_${version} --strip-components 1
   cd otp_src_${version}
   export ERL_TOP=$PWD
+
+  # Add target flag and xcomp flags if we are building for Apple Silicon
+  if [[ $APPLE_SILICON == 1 ]]
+  then
+    # we need a bootstrap an install first on our native platform
+    echo "We are cross compiling for Apple Silicon, bootstrapping!"
+    ./configure --enable-bootstrap-only
+    make -j
+
+    # set cross comp flags and compiler target flags
+    export CC="$CC -target arm64-apple-macos11"
+    export OTP_BUILD_FLAGS="$OTP_BUILD_FLAGS --host=arm64-apple-darwin --build=x86_64-apple-darwin"
+    export arch="darwin-arm64"
+  fi
+
+  # do the real OTP build
   ./otp_build setup -a $OTP_BUILD_FLAGS 1>&2
   cd ..
 fi
 
+##########
+## Release archive
+
 cd otp_src_${version}
 export ERL_TOP=$PWD
-release=$(echo otp-${version}-$(uname -sm | tr '[:upper:]' '[:lower:]' | tr ' ' '-'))
+release=$(echo otp-${version}-$arch)
 
 export RELEASE_ROOT=$TMPDIR/${release}
 make release -j$(getconf _NPROCESSORS_ONLN) 1>&2
-
-# TODO: disabling for now as it unnecessarily regenerates chunks, we already have them from the
-# pre-compiled source!
-# make release_docs -j$(getconf _NPROCESSORS_ONLN) DOC_TARGETS="chunks"
 
 cd $TMPDIR
 echo "creating ${TMPDIR}/${release}.tar.gz"
